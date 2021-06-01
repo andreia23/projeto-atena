7a8a5a0c8959c9aef467dabeadf50642cb22a580
Lucas Rodrigues <lucasrafaelor@hotmail.com>
2021-06-01 18:36:10 -0300

    listar alunos v0

--- src/main/java/br/edu/ifpb/pweb2/atena/business/service/AlunoService.java
+++ src/main/java/br/edu/ifpb/pweb2/atena/business/service/AlunoService.java
@@ -1,5 +1,28 @@
 package br.edu.ifpb.pweb2.atena.business.service;
 
-public class AlunoService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.edu.ifpb.pweb2.atena.business.model.Aluno;
+import br.edu.ifpb.pweb2.atena.business.repository.AlunoRepository;
+
+@Service
+public class AlunoService {
+	
+	private AlunoRepository alunoRepository;
+	
+	@Autowired
+	public AlunoService(AlunoRepository repository)
+	{
+		this.alunoRepository = repository;
+	}
+	
+	@Transactional
+	public List<Aluno> findAll()
+	{
+		return this.alunoRepository.findAll();
+	}
 }
--- src/main/java/br/edu/ifpb/pweb2/atena/controller/AlunoController.java
+++ src/main/java/br/edu/ifpb/pweb2/atena/controller/AlunoController.java
@@ -1,5 +1,33 @@
 package br.edu.ifpb.pweb2.atena.controller;
 
-public class AlunoController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.edu.ifpb.pweb2.atena.business.model.Aluno;
+import br.edu.ifpb.pweb2.atena.business.service.AlunoService;
+
+
+@Controller
+@RequestMapping("/aluno")
+public class AlunoController {
+	
+	@Autowired
+	private AlunoService alunoservice;
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView getForm(ModelAndView modelAndView) {
+		modelAndView.setViewName("aluno/list");
+		try {
+			List<Aluno> alunos = alunoservice.findAll();
+			modelAndView.addObject("alunos", alunos);
+		} catch (Exception e) {
+			modelAndView.addObject("mensagem", e.getMessage());
+		}
+		return modelAndView;
+	}
 }
