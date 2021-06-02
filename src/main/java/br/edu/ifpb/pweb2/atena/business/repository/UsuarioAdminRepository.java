9fd4b9be7e4d1f46992f3f6eff272eba26f54a9e
andreia23 <andreialima.berto08@gmail.com>
2021-06-02 10:48:11 -0300

    class filter

--- src/main/java/br/edu/ifpb/pweb2/atena/business/repository/UsuarioAdminRepository.java
+++ src/main/java/br/edu/ifpb/pweb2/atena/business/repository/UsuarioAdminRepository.java
@@ -8,9 +8,7 @@ import br.edu.ifpb.pweb2.atena.business.model.UsuarioAdmin;
 
 @Repository
 public interface UsuarioAdminRepository extends JpaRepository<UsuarioAdmin, Integer> {
-	
-//	@Query("select c from Cliente c where c.cpf = ?1")
-	
+
 	@Query("select u from UsuarioAdmin u where u.nome = ?1")
 	public UsuarioAdmin findByName(String name);
 
