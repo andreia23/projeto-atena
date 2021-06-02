4f71efc2d8bc50051cb1fb862d67cac23fbec956
Lucas Rodrigues <lucasrafaelor@hotmail.com>
2021-06-02 20:56:12 -0300

    commit final

--- src/main/java/br/edu/ifpb/pweb2/atena/controller/RelatorioController.java
+++ src/main/java/br/edu/ifpb/pweb2/atena/controller/RelatorioController.java
@@ -4,9 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.edu.ifpb.pweb2.atena.business.model.Aluno;
